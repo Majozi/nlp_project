54b910a9f29cad8616a6b2722cf8e83f3428b153
Majozi <bonza.majozi@gmail.com>
2023-08-22 00:30:30 +0200

    Update app_streamlit.app.py

--- app_streamlit.app.py
+++ app_streamlit.app.py
@@ -1,82 +1,71 @@
 import streamlit as st
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification, Trainer, BertTokenizer, BertModel
-import pandas as pd
-import numpy as np
-import torch
-from sklearn.cluster import KMeans
-from scipy.spatial.distance import cdist
-from sklearn.decomposition import LatentDirichletAllocation
-from sklearn.feature_extraction.text import CountVectorizer
-import os
-import nltk
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-import re
-import io
 
-# Initialize summarization models
-bart_summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-t5_summarizer = pipeline("summarization", model="t5-base")
+# Top Navigation
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", ['Getting Started', 'Summarization', 'Sentiment', 'Toxicity', 'N-Grams (Thematic)', 'Text Classification', 'Topic Modelling'])
 
-# Define functions
-def summarize_with_bart(text, min_length, max_length):
-    summary = bart_summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
-    return summary
+# Getting Started Page
+if selection == 'Getting Started':
+    st.title('Getting Started with Large Language Models')
+    st.write("""
+    Large language models, particularly transformers, have revolutionized natural language processing (NLP). 
+    They enable various applications like translation, summarization, sentiment analysis, and more. 
+    Transformers are deep learning models that process words in relation to all other words in a sentence,
+    capturing complex relationships and structures. They're pre-trained on vast datasets and can be fine-tuned
+    for specific tasks. This introduction provides an overview of their capabilities and uses.
+    """)
 
-def summarize_with_t5(text, min_length, max_length):
-    summary = t5_summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
-    return summary
+# Summarization Page
+elif selection == 'Summarization':
+    st.title('Text Summarization')
+    st.write("""
+    Text summarization is the process of condensing a larger piece of text into a concise summary. 
+    It helps in extracting the essential information from a document, preserving only the most 
+    critical points. Summarization techniques can be abstractive or extractive, with large language 
+    models playing a significant role in generating human-like summaries.
+    """)
 
-# Page title and sidebar
-st.title("NLP Analysis")
-page = st.sidebar.selectbox("Choose an analysis", ["Home", "Summarize", "Thematic", "Classification", "Topic Modelling", "Sentiment", "FAQs", "Toxicity"])
+# Sentiment Page
+elif selection == 'Sentiment':
+    st.title('Sentiment Analysis')
+    st.write("""
+    Sentiment analysis refers to the use of natural language processing to identify and categorize the sentiment
+    expressed in a piece of text. It can detect whether the sentiment is positive, negative, or neutral, and is widely
+    used in social media monitoring, customer feedback, and market research.
+    """)
 
-# Home page
-if page == "Home":
-    st.write("Welcome to the NLP Analysis tool!")
+# Toxicity Page
+elif selection == 'Toxicity':
+    st.title('Toxicity Detection')
+    st.write("""
+    Toxicity detection is essential in moderating online discussions. It involves identifying 
+    and filtering out toxic or harmful content, such as hate speech, abusive language, or misinformation. 
+    Machine learning models, including transformers, have become vital tools in automating this process.
+    """)
 
-# Summarize page
-elif page == "Summarize":
-    st.write("Summarize")
-    uploaded_file = st.file_uploader("Choose an Excel file")
-    if uploaded_file:
-        df = pd.read_excel(uploaded_file)
-        df = df[['text']].dropna()
-        text = df['text'].tolist()
-        min_length = st.number_input("Minimum Length", value=50)
-        max_length = st.number_input("Maximum Length", value=200)
-        bart_summary = summarize_with_bart(text, min_length, max_length)
-        t5_summary = summarize_with_t5(text, min_length, max_length)
-        st.write("BART Summary:", bart_summary)
-        st.write("T5 Summary:", t5_summary)
+# N-Grams (Thematic) Page
+elif selection == 'N-Grams (Thematic)':
+    st.title('N-Grams (Thematic) Analysis')
+    st.write("""
+    N-Grams are continuous sequences of n items from a given text or speech. Thematic analysis using N-Grams 
+    helps in understanding the context, themes, and frequently occurring patterns in a text. It's a useful 
+    technique in text mining and natural language processing.
+    """)
 
-# Thematic page
-elif page == "Thematic":
-    st.write("Thematic Analysis")
-    # Add your thematic analysis code here
+# Text Classification Page
+elif selection == 'Text Classification':
+    st.title('Text Classification')
+    st.write("""
+    Text classification is the task of categorizing text into predefined classes or labels. 
+    It includes applications like spam detection, topic labeling, sentiment analysis, and more. 
+    Machine learning models, especially large language models, have excelled in this domain.
+    """)
 
-# Classification page
-elif page == "Classification":
-    st.write("Classification")
-    # Add your classification code here
-
-# Topic Modelling page
-elif page == "Topic Modelling":
-    st.write("Topic Modelling")
-    # Add your topic modelling code here
-
-# Sentiment page
-elif page == "Sentiment":
-    st.write("Sentiment Analysis")
-    # Add your sentiment analysis code here
-
-# FAQs page
-elif page == "FAQs":
-    st.write("Frequently Asked Questions")
-    # Add your FAQs content here
-
-# Toxicity page
-elif page == "Toxicity":
-    st.write("Toxicity Analysis")
-    # Add your toxicity analysis code here
+# Topic Modelling Page
+elif selection == 'Topic Modelling':
+    st.title('Topic Modelling')
+    st.write("""
+    Topic modeling is a technique to discover the hidden thematic structure in a large collection of documents.
+    It's incredibly useful in organizing, understanding and summarizing large datasets of textual information.
+    Algorithms like LDA (Latent Dirichlet Allocation) are commonly used for this purpose.
+    """)
