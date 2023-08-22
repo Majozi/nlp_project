1d2c65459e7d15760fe50946131f1b3b982886b2
Majozi <bonza.majozi@gmail.com>
2023-08-22 21:06:15 +0200

    Create text-classification.py

---
+++ text-classification.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+from sklearn.cluster import KMeans
+from transformers import pipeline, BertTokenizer, BertModel
+import torch
+import numpy as np
+from scipy.spatial.distance import cdist
+import base64
+
+def classification():
+    candidate_labels_input = st.text_input("Enter candidate labels, separated by commas (e.g., positive,negative):")
+    candidate_labels = candidate_labels_input.split(",") if candidate_labels_input else []
+
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file:
+        df = pd.read_excel(uploaded_file)
+        df = df[['text']].dropna()
+
+        if not candidate_labels:
+            candidate_labels = ["positive", "negative"]
+
+        classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
+
+        res = classifier(df['text'].tolist(), candidate_labels=candidate_labels)
+        
+        # The rest of the code is similar to the Flask version, processing the results and displaying them
+
+        # ...
+
+        # Assuming you have the final DataFrames ready, you can display them as tables:
+        st.write(classified_text_neg_pos_sample)
+        st.write(classified_text_normalized_neg_pos)
+        st.write(bert_classification_sample)
+
+        # You can also provide download links for the Excel files:
+        # ...
+
+if __name__ == '__main__':
+    classification()
