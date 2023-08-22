98ee70ee20fa76f57f6fa3c926b7fd4fae879c8b
Majozi <bonza.majozi@gmail.com>
2023-08-22 23:40:05 +0200

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -1,92 +1,86 @@
 import streamlit as st
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+import matplotlib.pyplot as plt
 import pandas as pd
 
-# Text Classification
-def classify_text(text):
-    classifier = pipeline("text-classification", model="bert-base-uncased")
-    classification = classifier(text)
-    return classification
+# Title and Introduction about NLP
+st.title('NLP Analysis App')
+st.write("""
+Natural Language Processing (NLP) is a field of artificial intelligence that enables computers to understand, interpret, and generate human language. This technology allows machines to interact with text data, perform various analyses, and extract meaningful insights. The following app provides different NLP techniques to analyze and process text data.
+""")
 
-# Toxicity Analysis
-def analyze_toxicity(text):
-    toxicity_analyzer = pipeline("text-classification", model="unitary/toxic-bert") # Replace with a toxicity model
-    toxicity_analysis = toxicity_analyzer(text)
-    return toxicity_analysis
+# Text Classification
+def classify_text(text, labels):
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    classification = classifier(text, labels)
+    return classification
 
 # Summarization
 def summarize_text(text):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     summarized_text = summarizer(text)[0]['summary_text']
-    return summarized_text
+    negative_count = summarized_text.count('negative')
+    positive_count = summarized_text.count('positive')
+    return summarized_text, negative_count, positive_count
 
 # Topic Modeling
 def perform_topic_modeling(text):
     vectorizer = CountVectorizer()
     text_vectorized = vectorizer.fit_transform([text])
-    lda_model = LatentDirichletAllocation(n_components=5)
+    best_topics = 3 # Customize the logic to find the best number of topics
+    lda_model = LatentDirichletAllocation(n_components=best_topics)
     lda_model.fit(text_vectorized)
     topics = vectorizer.get_feature_names_out()
     return topics
 
 # Thematic Analysis (n-grams)
 def perform_thematic_analysis(text):
-    vectorizer = CountVectorizer(ngram_range=(2,2)) # 2-grams
+    vectorizer = CountVectorizer(ngram_range=(2,2))
     ngrams = vectorizer.fit_transform([text])
     ngrams_list = vectorizer.get_feature_names_out()
-    return ngrams_list
-
-# Sentiment Analysis
-def perform_sentiment_analysis(text):
-    model_name = "siebert/sentiment-roberta-large-english"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    sentiment_analyzer = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-    sentiment_results = sentiment_analyzer(text)
-    return sentiment_results
-
-# Rest of the Streamlit code remains the same
-
-
-# Title
-st.title('NLP Analysis App')
+    ngrams_count = ngrams.sum(axis=0)
+    return ngrams_list, ngrams_count
 
 # File uploader
 uploaded_file = st.file_uploader("Choose a text file", type=['txt', 'csv', 'xlsx'])
 
 # Operation selector
-operation = st.selectbox('Choose an operation:', ['Text Classification', 'Toxicity Analysis', 'Summarization', 'Topic Modeling', 'Thematic Analysis (n-grams)', 'Sentiment Analysis'])
+operation = st.selectbox('Choose an operation:', ['Text Classification', 'Summarization', 'Topic Modeling', 'Thematic Analysis (n-grams)'])
 
 # Trigger buttons
 if st.button('Start Analysis'):
     if uploaded_file is not None:
         # Read the uploaded file
-        if uploaded_file.type == 'text/plain':
-            uploaded_text = uploaded_file.read().decode()
-        elif uploaded_file.type in ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv']:
-            df = pd.read_excel(uploaded_file) if uploaded_file.type.endswith('sheet') else pd.read_csv(uploaded_file)
-            uploaded_text = ' '.join(df['text'].dropna().astype('str')) # Assuming the text is in the 'text' column
+        uploaded_text = uploaded_file.read().decode()
 
         st.write(f"Performing {operation}...")
         if operation == 'Text Classification':
-            classified_text = classify_text(uploaded_text)
+            user_labels = st.text_input("Enter the labels for classification (comma-separated):")
+            labels = user_labels.split(',')
+            classified_text = classify_text(uploaded_text, labels)
             st.write("Classified Text:", classified_text)
-        elif operation == 'Toxicity Analysis':
-            toxicity_analysis = analyze_toxicity(uploaded_text)
-            st.write("Toxicity Analysis:", toxicity_analysis)
         elif operation == 'Summarization':
-            summarized_text = summarize_text(uploaded_text)
+            summarized_text, negative_count, positive_count = summarize_text(uploaded_text)
             st.write("Summarized Text:", summarized_text)
+            fig, ax = plt.subplots()
+            ax.pie([negative_count, positive_count], labels=['Negative', 'Positive'], autopct='%1.1f%%')
+            st.pyplot(fig)
         elif operation == 'Topic Modeling':
             topics = perform_topic_modeling(uploaded_text)
             st.write("Topics:", topics)
         elif operation == 'Thematic Analysis (n-grams)':
-            thematic_analysis = perform_thematic_analysis(uploaded_text)
-            st.write("Thematic Analysis:", thematic_analysis)
-        elif operation == 'Sentiment Analysis':
-            sentiment_results = perform_sentiment_analysis(uploaded_text)
-            st.write("Sentiment Analysis Results:", sentiment_results)
+            ngrams_list, ngrams_count = perform_thematic_analysis(uploaded_text)
+            st.write("N-grams Identified:", ngrams_list)
+            st.write("Frequency Count:", ngrams_count)
     else:
         st.write("Please upload a file first.")
+
+# NLP Techniques Table
+nlp_techniques = pd.DataFrame({
+    'Analysis Type': ['Text Classification', 'Summarization', 'Topic Modeling', 'Thematic Analysis (n-grams)'],
+    'Summary': ['Classify text into user-defined labels', 'Summarize long texts into concise summaries', 'Identify main topics in a text', 'Analyze frequent word combinations (n-grams)'],
+    'Suggestions on how to use': ['Categorize documents, Sentiment analysis', 'Content summarization, Highlight generation', 'Content categorization, Theme discovery', 'Text pattern analysis, Keyword extraction']
+})
+st.table(nlp_techniques)
