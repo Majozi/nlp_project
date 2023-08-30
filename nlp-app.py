a3e830cb360ac8b5488baeceea64662812edb931
Majozi <bonza.majozi@gmail.com>
2023-08-30 19:52:02 +0200

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -18,13 +18,12 @@ nltk.download('wordnet')
 
 
 
-# Function for thematic analysis
-def thematic_analysis(file, ngram_range):
-    df = pd.read_excel(file)
+def thematic_analysis(file):
+    df = pd.read_excel(file)  # Reading the Excel file
     df = df[['text']].dropna()
 
     stoplist = stopwords.words('english')
-    c_vec = CountVectorizer(stop_words=stoplist, ngram_range=ngram_range)
+    c_vec = CountVectorizer(stop_words=stoplist, ngram_range=(2, 4))
     ngrams = c_vec.fit_transform(df['text'])
     count_values = ngrams.toarray().sum(axis=0)
     vocab = c_vec.vocabulary_
@@ -191,7 +190,7 @@ elif selection == 'Topic Modelling':
 elif selection == 'N-Grams (Thematic)':
     st.title("Thematic Analysis Using N-Grams")
     uploaded_file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
-
+    
     if uploaded_file is not None:
         df_ngram = thematic_analysis(uploaded_file)
         st.write(df_ngram)
