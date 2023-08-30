07dd2bd47ba0210fa9f6e8a2ce15a9ab9c9e7658
Majozi <bonza.majozi@gmail.com>
2023-08-30 20:25:46 +0200

    Update thematic.py

--- thematic.py
+++ thematic.py
@@ -8,29 +8,35 @@ from nltk.corpus import stopwords
 import nltk
 nltk.download('stopwords')
 
-def thematic_analysis(file):
+def thematic_analysis(file, ngram_min, ngram_max):
     df = pd.read_excel(file)  # Reading the Excel file
     df = df[['text']].dropna()
 
     stoplist = stopwords.words('english')
-    c_vec = CountVectorizer(stop_words=stoplist, ngram_range=(2, 4))
+    c_vec = CountVectorizer(stop_words=stoplist, ngram_range=(ngram_min, ngram_max))
     ngrams = c_vec.fit_transform(df['text'])
     count_values = ngrams.toarray().sum(axis=0)
     vocab = c_vec.vocabulary_
 
     df_ngram = pd.DataFrame(sorted([(count_values[i], k) for k, i in vocab.items()],
-                                   reverse=True)).rename(columns={0: 'frequency', 1: 'bigram/trigram'})
+                                   reverse=True)).rename(columns={0: 'frequency', 1: 'ngram'})
 
     return df_ngram
 
 # Main app
 st.title("Thematic Analysis Using N-Grams")
 uploaded_file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
+
+# Sliders for n-gram range
+ngram_min = st.slider("Minimum N-gram Range", 1, 5, 2)
+ngram_max = st.slider("Maximum N-gram Range", ngram_min, 5, 4)  # Make sure max is always >= min
+
 st.write("""
 Group the items in the table below to get the themes. To flesh them out in your discussion, go back to your original data and search these words to get more insight. When creating a theme, 
-remember to get a sum of all the bigrams/trigrams that you combined so that you may Quantify your argument. PLEASE NOTE THIS: The table below doesn't represent the number of responses, but the number of times the
-bigrams/trigrams occur on your data.
+remember to get a sum of all the bigrams/trigrams that you combined so that you may quantify your argument. PLEASE NOTE THIS: The table below doesn't represent the number of responses, but the number of times the
+n-grams occur in your data.
 """)
+
 if uploaded_file is not None:
-    df_ngram = thematic_analysis(uploaded_file)
+    df_ngram = thematic_analysis(uploaded_file, ngram_min, ngram_max)
     st.write(df_ngram)
