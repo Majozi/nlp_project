daea2775406c4eacccbc9ca00ee40fb70ca92e3d
Majozi <bonza.majozi@gmail.com>
2023-08-30 23:15:53 +0200

    Update text-classification.py

--- text-classification.py
+++ text-classification.py
@@ -4,32 +4,36 @@ from transformers import pipeline
 
 # Function to process the text and classify it
 def classify_text(file, candidate_labels):
-    df = pd.read_excel(file)
-    df = df[['text']].dropna()
+    try:
+        df = pd.read_excel(file)
+        df = df[['text']].dropna()
 
-    if not candidate_labels:
-        candidate_labels = ["positive", "negative", "neutral"]
+        if not candidate_labels:
+            candidate_labels = ["positive", "negative", "neutral"]
 
-    classifier = pipeline(
-        task="zero-shot-classification",
-        model="facebook/bart-large-mnli"
-    )
+        classifier = pipeline(
+            task="zero-shot-classification",
+            model="facebook/bart-large-mnli"
+        )
 
-    res = classifier(df['text'].tolist(), candidate_labels=candidate_labels)
+        res = classifier(df['text'].tolist(), candidate_labels=candidate_labels)
 
-    labels = []
-    seq = []
-    scores = []
+        labels = []
+        seq = []
+        scores = []
 
-    for item in res:
-        labels.append(item['labels'][0])  # Assuming the first label is the most likely one
-        seq.append(item['sequence'])
-        scores.append(item['scores'][0])  # Assuming the first score is the most likely one
+        for item in res:
+            labels.append(item['labels'][0])
+            seq.append(item['sequence'])
+            scores.append(item['scores'][0])
 
-    classified_text = pd.DataFrame(list(zip(seq, labels, scores)), columns=['Text', 'Label', 'Score'])
-    classified_text_normalized = pd.DataFrame(classified_text['Label'].value_counts(normalize=True))
+        classified_text = pd.DataFrame(list(zip(seq, labels, scores)), columns=['Text', 'Label', 'Score'])
+        classified_text_normalized = pd.DataFrame(classified_text['Label'].value_counts(normalize=True))
 
-    return classified_text, classified_text_normalized
+        return classified_text, classified_text_normalized
+    except Exception as e:
+        st.write(f"An error occurred: {e}")
+        return None, None
 
 # Streamlit App
 st.title('Text Classification')
@@ -44,8 +48,10 @@ candidate_labels = [x.strip() for x in candidate_labels_input.split(",")]
 if file:
     classified_text, classified_text_normalized = classify_text(file, candidate_labels)
 
-    st.write("### Classified Text Sample")
-    st.table(classified_text.head(3))
+    if classified_text is not None:
+        st.write("### Classified Text Sample")
+        st.table(classified_text.head(3))
 
-    st.write("### Label Distribution")
-    st.bar_chart(classified_text_normalized)
+    if classified_text_normalized is not None:
+        st.write("### Label Distribution")
+        st.bar_chart(classified_text_normalized)
