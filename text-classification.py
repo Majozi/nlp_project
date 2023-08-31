55a4ba589daeff855a0300e8a2a3c4d3f41d8fbf
Majozi <bonza.majozi@gmail.com>
2023-08-31 09:07:00 +0200

    Update text-classification.py

--- text-classification.py
+++ text-classification.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from transformers import pipeline
+import matplotlib.pyplot as plt
 
 # Function to process the text and classify it
 def classify_text(file, candidate_labels):
@@ -51,16 +52,22 @@ st.title('Text Classification')
 file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
 
 if file:
-    # Input for candidate labels
-    candidate_labels_input = st.text_input("Candidate Labels (comma separated)", "positive,negative,neutral")
-    candidate_labels = [x.strip() for x in candidate_labels_input.split(",")]
+    with st.form(key='analysis_form'):
+        # Input for candidate labels
+        candidate_labels_input = st.text_input("Candidate Labels (comma separated)", "positive,negative,neutral")
+        
+        # Submit button
+        submitted = st.form_submit_button("Start Analysis")
 
-    classified_text, classified_text_normalized = classify_text(file, candidate_labels)
+    if submitted:
+        candidate_labels = [x.strip() for x in candidate_labels_input.split(",")]
 
-    if classified_text is not None:
-        st.write("### Classified Text Sample")
-        st.table(classified_text.head(3))
+        classified_text, classified_text_normalized = classify_text(file, candidate_labels)
 
-    if classified_text_normalized is not None:
-        st.write("### Label Distribution")
-        st.bar_chart(classified_text_normalized)
+        if classified_text is not None:
+            st.write("### Classified Text Sample")
+            st.table(classified_text.head(3))
+
+        if classified_text_normalized is not None:
+            st.write("### Label Distribution")
+            st.pyplot(classified_text_normalized.plot.pie(y='Label', autopct='%1.1f%%', legend=False))
