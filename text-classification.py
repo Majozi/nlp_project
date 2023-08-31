c99a2c450a8d4ff4b4770abfd534f556cfabda88
Majozi <bonza.majozi@gmail.com>
2023-08-31 08:27:08 +0200

    Update text-classification.py

--- text-classification.py
+++ text-classification.py
@@ -5,17 +5,24 @@ from transformers import pipeline
 # Function to process the text and classify it
 def classify_text(file, candidate_labels):
     try:
+        # Read the Excel file into a DataFrame
         df = pd.read_excel(file)
+        
+        # Check if 'text' column exists
+        if 'text' not in df.columns:
+            st.error("The Excel file must contain a 'text' column.")
+            return None, None
+
+        # Drop NaN values from the 'text' column
         df = df[['text']].dropna()
 
-        if not candidate_labels:
-            candidate_labels = ["positive", "negative", "neutral"]
-
+        # Initialize the zero-shot classifier
         classifier = pipeline(
             task="zero-shot-classification",
             model="facebook/bart-large-mnli"
         )
 
+        # Perform classification
         res = classifier(df['text'].tolist(), candidate_labels=candidate_labels)
 
         labels = []
@@ -27,25 +34,27 @@ def classify_text(file, candidate_labels):
             seq.append(item['sequence'])
             scores.append(item['scores'][0])
 
+        # Create a DataFrame to store the results
         classified_text = pd.DataFrame(list(zip(seq, labels, scores)), columns=['Text', 'Label', 'Score'])
         classified_text_normalized = pd.DataFrame(classified_text['Label'].value_counts(normalize=True))
 
         return classified_text, classified_text_normalized
+
     except Exception as e:
-        st.write(f"An error occurred: {e}")
+        st.error(f"An error occurred: {e}")
         return None, None
 
-# Streamlit App
+# Streamlit App UI
 st.title('Text Classification')
 
 # Upload the Excel file
 file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
 
-# Input for candidate labels
-candidate_labels_input = st.text_input("Candidate Labels (comma separated)", "positive,negative,neutral")
-candidate_labels = [x.strip() for x in candidate_labels_input.split(",")]
-
 if file:
+    # Input for candidate labels
+    candidate_labels_input = st.text_input("Candidate Labels (comma separated)", "positive,negative,neutral")
+    candidate_labels = [x.strip() for x in candidate_labels_input.split(",")]
+
     classified_text, classified_text_normalized = classify_text(file, candidate_labels)
 
     if classified_text is not None:
