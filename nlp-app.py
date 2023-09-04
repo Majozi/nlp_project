a91ac42396ba7e67f7fd3c18166ba1581dffa946
Majozi <bonza.majozi@gmail.com>
2023-09-04 23:17:48 +0200

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -16,6 +16,30 @@ import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
 
+
+# Initialize summarization models
+bart_summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+t5_summarizer = pipeline("summarization", model="t5-base")
+
+def summarize_with_bart(text, min_length, max_length):
+    summary = bart_summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
+    return summary
+
+def summarize_with_t5(text, min_length, max_length):
+    summary = t5_summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
+    return summary
+
+def summarize_text(text_list, min_length, max_length):
+    bart_summaries = []
+    t5_summaries = []
+
+    # Summarize each text in the list
+    for text in text_list:
+        bart_summaries.append(summarize_with_bart(text, min_length, max_length))
+        t5_summaries.append(summarize_with_t5(text, min_length, max_length))
+
+    return bart_summaries, t5_summaries
+
 # Initialize Zero-Shot Classification pipeline
 classifier = pipeline('zero-shot-classification', model='typeform/distilbert-base-uncased-mnli')
 
@@ -66,7 +90,7 @@ st.image(image_url, width=100)
 
 # Top Navigation
 st.sidebar.title('Text Analytics')
-selection = st.sidebar.radio("Go to", ['Getting Started', 'Sentiment', 'N-Grams (Thematic)', 'Text Classification', 'Topic Modelling'])
+selection = st.sidebar.radio("Go to", ['Getting Started', 'Summarization','Sentiment', 'N-Grams (Thematic)', 'Text Classification', 'Topic Modelling'])
 
 if selection == 'Getting Started':
     st.title("Natural Language Processing")
@@ -328,3 +352,34 @@ elif selection == 'N-Grams (Thematic)':
             )
     
             st.altair_chart(chart)  
+
+st.sidebar.title("Summarization Parameters")
+min_length = st.sidebar.slider("Minimum Length", 10, 100, 30)
+max_length = st.sidebar.slider("Maximum Length", 50, 300, 100)
+
+#Summarization
+elif selection == 'N-Grams (Summarization)':
+    st.title("Summarization")
+    st.write("""
+    
+    """)
+    uploaded_file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    if 'text' not in df.columns:
+        st.error("Please make sure the Excel file contains a 'text' column.")
+    else:
+        df = df[['text']].dropna().astype('str')
+        text_list = df['text'].tolist()
+
+        # Summarize the text
+        bart_summaries, t5_summaries = summarize_text(text_list, min_length, max_length)
+
+        result_df = pd.DataFrame({
+            "Original Text": text_list,
+            "BART Summary": bart_summaries,
+            "T5 Summary": t5_summaries
+        })
+
+        st.write(result_df)
