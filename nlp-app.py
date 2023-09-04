6204df04cea2e1a5886feda5c915036d9c96a564
Majozi <bonza.majozi@gmail.com>
2023-09-04 22:24:52 +0200

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -16,6 +16,9 @@ import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
 
+# Initialize Zero-Shot Classification pipeline
+classifier = pipeline('zero-shot-classification', model='typeform/distilbert-base-uncased-mnli')
+
 # Downloading the NLTK resources if not downloaded
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -75,7 +78,7 @@ Natural Language Processing (NLP) is a multifaceted field that integrates comput
 elif selection == 'Sentiment':
     st.title("Sentiment Analysis")
 
-    # Text Classification pipeline
+    
     classifier = pipeline('sentiment-analysis')
     
     st.write("""
@@ -188,6 +191,78 @@ elif selection == 'Topic Modelling':
 
         topics = display_topics(model, tf_feature_names, no_top_words)
         st.write(topics)
+
+#Text Classification
+elif selection == 'Text Classification':
+    st.title('Text Classification App')
+    st.write("""
+    The model used for this classification is **typeform/distilbert-base-uncased-mnli**. Ensure that you have done good groundwork in
+    identifying the recurring ideas from the text. This classifier takes either single words or short phases that are separated by a comma. \n \n
+    **TIPS FOR USAGE:** \n
+    First, run the word cloud and the N-GRAM analysis so you can identify the big ideas. You can modify the words / phrases to get the best results. \n
+    When breaking the data into fragments, just be aware that the analysis will be based on the fragment and may not be representative of the whole part.
+    """)
+    # Define user input for Classification Labels
+    labels = st.text_input(' **Enter your classification labels, separated by comma** ')
+    
+    # Split the labels into a list
+    labels = [label.strip() for label in labels.split(',')]
+    
+    # File Upload
+    uploaded_file = st.file_uploader("Upload CSV or Excel with a column 'text' ", type=['csv', 'xlsx'])
+    
+    if uploaded_file is not None:
+        try:
+            df = pd.read_csv(uploaded_file)
+            if df.empty:
+                st.markdown('Uploaded file is empty. Please upload another.')
+                raise ValueError("Empty File")
+        except Exception as e:
+            try:
+                df = pd.read_excel(uploaded_file)
+                if df.empty:
+                    st.markdown('Uploaded file is empty. Please upload another.')
+                    raise ValueError("Empty File")
+            except Exception as e:
+                st.markdown(f'Error reading file: {e}')
+                raise
+    
+        # Drop empty rows based on the 'text' column
+        df.dropna(subset=['text'], inplace=True)
+    
+        if 'text' not in df.columns:
+            st.markdown('File does not have a `text` column. Please upload another.')
+        elif not labels:
+            st.markdown('Please enter some classification labels.')
+        else:
+            def classify(text, labels):
+                result = classifier(text, labels)
+                return pd.Series([result['labels'][0], result['scores'][0]], index=['label', 'score'])
+            
+            df[['label', 'score']] = df['text'].apply(lambda x: classify(x, labels))
+            
+            # Normalize the labels and calculate frequency distribution
+            label_counts = df['label'].value_counts(normalize=True)
+            
+            # Sort by frequency
+            label_counts = label_counts.sort_values(ascending=False)
+            
+            # Pie chart
+            fig, ax = plt.subplots()
+            ax.pie(label_counts, labels=label_counts.index, autopct='%1.1f%%', startangle=90, colors=['#005baa', '#c48939', '#d61c33'])
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+            
+            # Remove background
+            ax.set_facecolor('none')
+            fig.patch.set_visible(False)
+            
+            st.pyplot(fig)
+    
+            st.write(df)
+    
+
+
+    
 
 elif selection == 'N-Grams (Thematic)':
     st.title("Thematic Analysis Using N-Grams")
