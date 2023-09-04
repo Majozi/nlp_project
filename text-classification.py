2195da8880c900ba6b9424a8fc95286662a92930
Majozi <bonza.majozi@gmail.com>
2023-09-04 21:40:27 +0200

    Update text-classification.py

--- text-classification.py
+++ text-classification.py
@@ -1,50 +1,36 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 from transformers import pipeline
 
-# Title and description
-st.title("Zero-Shot Classification Streamlit App")
-st.write("Upload a CSV or Excel file with a column named 'text' to get zero-shot classification results.")
+# Zero-Shot Classification pipeline
+classifier = pipeline('zero-shot-classification', model='typeform/distilbert-base-uncased-mnli')
 
-# Initialize zero-shot-classification pipeline
-classifier = pipeline("zero-shot-classification", model="distilbert-base-uncased-finetuned-mnli")
+# Streamlit App
+st.title('Text Classification App')
 
-# User-defined labels for classification
-label1 = st.text_input("Enter Label 1:")
-label2 = st.text_input("Enter Label 2:")
-label3 = st.text_input("Enter Label 3:")
-label4 = st.text_input("Enter Label 4:")
-label5 = st.text_input("Enter Label 5:")
+# Define user input for Classification Labels
+labels = st.text_input('Enter your classification labels, separated by comma')
 
-labels = [label for label in [label1, label2, label3, label4, label5] if label]
+# Split the labels into a list
+labels = [label.strip() for label in labels.split(',')]
 
-# Upload file
-uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
+# File Upload
+uploaded_file = st.file_uploader("Upload CSV or Excel", type=['csv', 'xlsx'])
 
-if uploaded_file:
+if uploaded_file is not None:
     try:
-        # Read the uploaded file into a DataFrame
-        if uploaded_file.name.endswith('.csv'):
-            df = pd.read_csv(uploaded_file)
-        elif uploaded_file.name.endswith('.xlsx'):
-            df = pd.read_excel(uploaded_file)
-        
-        # Check if 'text' column exists
-        if 'text' not in df.columns:
-            st.error("No 'text' column found in the uploaded file.")
-        else:
-            results_list = []
-            
-            # Perform zero-shot classification and store the results
-            for text in df['text']:
-                result = classifier(text, labels)
-                results_list.append(result['labels'][0])  # Most likely label
-                
-            # Add the results to the DataFrame
-            df['classification_result'] = results_list
-            
-            # Display the updated DataFrame
-            st.write(df)
-            
+        df = pd.read_csv(uploaded_file)
     except Exception as e:
-        st.error(f"An error occurred: {e}")
+        df = pd.read_excel(uploaded_file)
+
+    if 'text' not in df.columns:
+        st.markdown('File does not have a `text` column. Please upload another.')
+    else:
+        def classify(text, labels):
+            result = classifier(text, labels)
+            return pd.Series([result['labels'][0], result['scores'][0]], index=['label', 'score'])
+        
+        res_df = df['text'].apply(lambda x: classify(x, labels))
+        df = pd.concat([df, res_df], axis=1)
+        
+        st.write(df)
