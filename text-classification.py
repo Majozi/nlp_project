5d692e55b7e866f6de4f3fb39cd3652c83ea9b1d
Majozi <bonza.majozi@gmail.com>
2023-09-04 21:44:10 +0200

    Update text-classification.py

--- text-classification.py
+++ text-classification.py
@@ -2,10 +2,10 @@ import pandas as pd
 import streamlit as st
 from transformers import pipeline
 
-# Zero-Shot Classification pipeline
+# Initialize Zero-Shot Classification pipeline
 classifier = pipeline('zero-shot-classification', model='typeform/distilbert-base-uncased-mnli')
 
-# Streamlit App
+# Streamlit App Title
 st.title('Text Classification App')
 
 # Define user input for Classification Labels
@@ -20,17 +20,30 @@ uploaded_file = st.file_uploader("Upload CSV or Excel", type=['csv', 'xlsx'])
 if uploaded_file is not None:
     try:
         df = pd.read_csv(uploaded_file)
+        if df.empty:
+            st.markdown('Uploaded file is empty. Please upload another.')
+            raise ValueError("Empty File")
     except Exception as e:
-        df = pd.read_excel(uploaded_file)
+        try:
+            df = pd.read_excel(uploaded_file)
+            if df.empty:
+                st.markdown('Uploaded file is empty. Please upload another.')
+                raise ValueError("Empty File")
+        except Exception as e:
+            st.markdown(f'Error reading file: {e}')
+            raise
+
+    # Drop empty rows based on the 'text' column
+    df.dropna(subset=['text'], inplace=True)
 
     if 'text' not in df.columns:
         st.markdown('File does not have a `text` column. Please upload another.')
+    elif not labels:
+        st.markdown('Please enter some classification labels.')
     else:
         def classify(text, labels):
             result = classifier(text, labels)
             return pd.Series([result['labels'][0], result['scores'][0]], index=['label', 'score'])
         
-        res_df = df['text'].apply(lambda x: classify(x, labels))
-        df = pd.concat([df, res_df], axis=1)
-        
+        df[['label', 'score']] = df['text'].apply(lambda x: classify(x, labels))
         st.write(df)
