ccfba8e90ac3fc2dcdd4d5ffb5d574d0292f4ef3
Majozi <bonza.majozi@gmail.com>
2023-09-04 21:14:51 +0200

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -12,12 +12,29 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import Trainer
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
 
 # Downloading the NLTK resources if not downloaded
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-# Function to download data as a CSV file
+# Function to make DataFrame downloadable
+def get_table_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    return f'<a href="data:file/csv;base64,{b64}" download="classified_text.csv">Download CSV File</a>'
+
+# Function to make Pie Chart downloadable
+def get_pie_chart_download_link(fig):
+    buffer = BytesIO()
+    fig.savefig(buffer, format="png")
+    buffer.seek(0)
+    b64 = base64.b64encode(buffer.read()).decode()
+    return f'<a href="data:image/png;base64,{b64}" download="pie_chart.png">Download Pie Chart</a>'
+
+# Function to download N-GRAMS data as a CSV file
 def download_csv(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
@@ -58,81 +75,97 @@ Natural Language Processing (NLP) is a multifaceted field that integrates comput
 elif selection == 'Sentiment':
     st.title("Sentiment Analysis")
 
-    uploaded_file = st.file_uploader("Choose an Excel file", type=['xlsx'])
-
+        # Text Classification pipeline
+    classifier = pipeline('sentiment-analysis')
+    
+    # Streamlit App
+    st.title('Text Classification App')
+    st.write("""
+             **TIPS FOR USE:** \n After the analysis has been completed, download the table below and read through a sample of responses to get a feel of
+             how accurate the classification was. A rule of thumb is to always pay attention to rows where the 
+             score is below 50 and the sentiment is negative and the score is close to 100%. \n \n
+             An overall positive sentiment that is below 50% (depending on the question asked), may be
+             an immediate indicator of issues to be addressed. To find these, filter for a score above 75%
+             and a negative label. \n \n
+             Be mindful that you may have to reclassify and then recalculate the overall sentiment.
+             """)
+    
+    # File Upload
+    uploaded_file = st.file_uploader("Upload CSV or Excel with a column name 'text'.", type=['csv', 'xlsx'])
+    
     if uploaded_file is not None:
-        df = pd.read_excel(uploaded_file)
+        try:
+            df = pd.read_csv(uploaded_file)
+        except Exception as e:
+            df = pd.read_excel(uploaded_file)
     
-        local_csv_dataset = df[['text']]
-        pred_texts = local_csv_dataset.dropna().astype('str')
-        pred_texts = pred_texts['text'].tolist()
+        if 'text' not in df.columns:
+            st.markdown('File does not have a `text` column. Please upload another.')
+        else:
+            # Perform Text Classification
+            df['label'] = df['text'].apply(lambda x: classifier(x)[0]['label'])
+            df['score'] = df['text'].apply(lambda x: classifier(x)[0]['score']) * 100
     
-        model_name = "siebert/sentiment-roberta-large-english"
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        trainer = Trainer(model=model)
+            st.write(df)
     
-        tokenized_texts = tokenizer(pred_texts, truncation=True, padding=True)
+            # Download DataFrame
+            st.markdown(get_table_download_link(df), unsafe_allow_html=True)
     
-        class SimpleDataset:
-            def __init__(self, tokenized_texts):
-                self.tokenized_texts = tokenized_texts
+            # Sentiment Percentage
+            sentiment_counts = df['label'].value_counts(normalize=True)
     
-            def __len__(self):
-                return len(self.tokenized_texts["input_ids"])
+            # Define colors
+            colors = ['#005baa' if label == 'POSITIVE' else '#c48939' for label in sentiment_counts.index]
     
-            def __getitem__(self, idx):
-                return {k: v[idx] for k, v in self.tokenized_texts.items()}
+            # Pie Chart
+            fig, ax = plt.subplots()
+            ax.pie(sentiment_counts, labels=sentiment_counts.index, colors=colors, autopct='%1.1f%%', startangle=90)
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     
-        pred_dataset = SimpleDataset(tokenized_texts)
-        predictions = trainer.predict(pred_dataset)
-        preds = predictions.predictions.argmax(-1)
-        labels = pd.Series(preds).map(model.config.id2label)
-        scores = (np.exp(predictions[0]) / np.exp(predictions[0]).sum(-1, keepdims=True)).max(1)
-        sentiment = pd.DataFrame(list(zip(pred_texts, preds, labels, scores)), columns=['text', 'Prediction', 'Label', 'Score'])
+            st.pyplot(fig)
     
-        # Display the sentiment analysis results
-        st.write(sentiment)
-
-# Text Classification Page
-elif selection == 'Text Classification':
-    st.title('Text Classification')
-
-    candidate_labels_input = st.text_input("Enter candidate labels, separated by commas (e.g., positive,negative,neutral):")
-    candidate_labels = candidate_labels_input.split(",") if candidate_labels_input else []
-
-    uploaded_file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
-
-    if uploaded_file:
-        df = pd.read_excel(uploaded_file)
-        df = df[['text']].dropna()
-
-        if not candidate_labels:
-            # If no candidate labels are provided, use default labels
-            candidate_labels = ["positive", "negative", "neutral"]
-
-        classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
-
-        res = classifier(df['text'].tolist(), candidate_labels=candidate_labels)
-
-        def make_table_data(res_list):
-            labels = []
-            seq = []
-            scores = []
-            for item in res_list:
-                labels.append(item['labels'][0])
-                seq.append(item['sequence'])
-                scores.append(item['scores'][0])
-
-            return seq, labels, scores
-
-        seq, labels, scores = make_table_data(res)
-
-        classified_text_neg_pos = pd.DataFrame(list(zip(seq, labels, scores)), columns=['Text', 'Label', 'Score'])
-        classified_text_normalized_neg_pos = pd.DataFrame(classified_text_neg_pos['Label'].value_counts(normalize=True))
-
-        st.write(classified_text_neg_pos)
-        st.write(classified_text_normalized_neg_pos)
+            # Download Pie Chart
+            st.markdown(get_pie_chart_download_link(fig), unsafe_allow_html=True)
+    
+    # Text Classification Page
+    elif selection == 'Text Classification':
+        st.title('Text Classification')
+    
+        candidate_labels_input = st.text_input("Enter candidate labels, separated by commas (e.g., positive,negative,neutral):")
+        candidate_labels = candidate_labels_input.split(",") if candidate_labels_input else []
+    
+        uploaded_file = st.file_uploader("Choose an Excel file containing 'text' column", type="xlsx")
+    
+        if uploaded_file:
+            df = pd.read_excel(uploaded_file)
+            df = df[['text']].dropna()
+    
+            if not candidate_labels:
+                # If no candidate labels are provided, use default labels
+                candidate_labels = ["positive", "negative", "neutral"]
+    
+            classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
+    
+            res = classifier(df['text'].tolist(), candidate_labels=candidate_labels)
+    
+            def make_table_data(res_list):
+                labels = []
+                seq = []
+                scores = []
+                for item in res_list:
+                    labels.append(item['labels'][0])
+                    seq.append(item['sequence'])
+                    scores.append(item['scores'][0])
+    
+                return seq, labels, scores
+    
+            seq, labels, scores = make_table_data(res)
+    
+            classified_text_neg_pos = pd.DataFrame(list(zip(seq, labels, scores)), columns=['Text', 'Label', 'Score'])
+            classified_text_normalized_neg_pos = pd.DataFrame(classified_text_neg_pos['Label'].value_counts(normalize=True))
+    
+            st.write(classified_text_neg_pos)
+            st.write(classified_text_normalized_neg_pos)
 
 # Topic Modelling Page
 elif selection == 'Topic Modelling':
