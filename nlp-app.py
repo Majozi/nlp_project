4740e7f9385b2d6a12c44316e2330df82ba6ce62
Majozi <bonza.majozi@gmail.com>
2023-09-04 23:53:20 +0200

    Update nlp-app.py

--- nlp-app.py
+++ nlp-app.py
@@ -68,7 +68,7 @@ st.image(image_url, width=100)
 
 # Top Navigation
 st.sidebar.title('Text Analytics')
-selection = st.sidebar.radio("Go to", ['Getting Started', 'Summarization','Sentiment', 'N-Grams (Thematic)', 'Text Classification', 'Topic Modelling'])
+selection = st.sidebar.radio("Go to", ['Getting Started', 'Summarization', 'N-Grams (Thematic)', 'Text Classification', 'Topic Modelling'])
 
 if selection == 'Getting Started':
     st.title("Natural Language Processing")
