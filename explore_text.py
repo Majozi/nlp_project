97720f3e90fd2671baec9e7d6f446d06216cc5b4
Majozi <bonza.majozi@gmail.com>
2023-10-19 09:42:38 +0200

    Update explore_text.py

--- explore_text.py
+++ explore_text.py
@@ -8,9 +8,12 @@ import networkx as nx
 from collections import Counter
 from wordcloud import WordCloud
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.metrics.pairwise import cosine_similarity
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
+
+# Functions
 def is_meaningless(text):
     return bool(re.fullmatch(r'[0-9\s\W]*', text))
 
@@ -18,20 +21,43 @@ def extractive_summarize(text):
     sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
     return sentences[0] if sentences else "No text to summarize"
 
+# Function to plot the graph
 def plot_graph(filtered_feedback, cosine_sim, threshold):
     G = nx.Graph()
-    for i, feedback in enumerate(filtered_feedback):
-        G.add_node(i, text=feedback)
+    for i in range(len(filtered_feedback)):
+        G.add_node(i, text=filtered_feedback[i])
+
     for i in range(len(filtered_feedback)):
         for j in range(i+1, len(filtered_feedback)):
             if cosine_sim[i, j] > threshold:
                 G.add_edge(i, j, weight=cosine_sim[i, j])
+
     plt.figure(figsize=(12, 12))
     pos = nx.spring_layout(G, seed=42)
     nx.draw(G, pos, with_labels=True, labels=nx.get_node_attributes(G, 'text'), node_color='skyblue', node_size=500, font_size=8, font_color='black', edge_color='gray')
     plt.title('Network Graph of Student Feedback About Tutor Based on Cosine Similarity')
     st.pyplot()
+
+# User Input for additional stopwords
+additional_stopwords = st.text_input("Enter additional stopwords separated by commas:")
+if additional_stopwords:
+    additional_stopwords = additional_stopwords.split(',')
+    additional_stopwords = [word.strip().lower() for word in additional_stopwords]
+
+default_stopwords = list(CountVectorizer(stop_words='english').get_stop_words())
+
+if additional_stopwords:
+    new_stopwords = set(default_stopwords).union(set(additional_stopwords))
+else:
+    new_stopwords = default_stopwords
+
+count_vectorizer = CountVectorizer(stop_words=new_stopwords)
+tfidf_vectorizer = TfidfVectorizer(stop_words=new_stopwords)
+
+# Streamlit App
 st.title('Text Analysis Dashboard')
+
+# File Upload
 uploaded_file = st.file_uploader("Upload your file (CSV or Excel)", type=["csv", "xlsx"])
 
 if uploaded_file:
@@ -43,66 +69,19 @@ if uploaded_file:
     if 'text' not in df.columns:
         st.error("The uploaded file does not contain a column named 'text'. Please upload a valid file.")
     else:
+        # Data Preprocessing
         feedback_text = df['text'].dropna().astype(str).tolist()
 
-        # User Input for additional stopwords
-        additional_stopwords = st.text_input("Enter additional stopwords separated by commas:")
-        if additional_stopwords:
-            additional_stopwords = additional_stopwords.split(',')
-            additional_stopwords = [word.strip().lower() for word in additional_stopwords]
-
-        count_vectorizer = CountVectorizer(stop_words='english')
-        tfidf_vectorizer = TfidfVectorizer(stop_words='english')
-        
-        if additional_stopwords:
-            new_stopwords = set(count_vectorizer.get_stop_words()).union(set(additional_stopwords))
-            count_vectorizer.set_params(stop_words=new_stopwords)
-            tfidf_vectorizer.set_params(stop_words=new_stopwords)
+        # 1. Word Cloud Analysis
         st.subheader('1. Word Cloud Analysis')
         wc_width = st.slider("Word Cloud Width", 400, 1200, 800)
         wc_height = st.slider("Word Cloud Height", 200, 800, 400)
-        
+
         filtered_words = ' '.join(feedback_text).split()
-        filtered_words = [word for word in filtered_words if word.lower() not in set(count_vectorizer.get_stop_words())]
+        filtered_words = [word for word in filtered_words if word.lower() not in set(new_stopwords)]
         word_freq = Counter(filtered_words)
         wordcloud = WordCloud(width=wc_width, height=wc_height, background_color='white').generate_from_frequencies(word_freq)
-        
+
         st.image(wordcloud.to_array(), caption='Word Cloud of Feedback', use_column_width=True)
-        st.subheader('2. Binned Word Count Analysis')
-        bin_labels = st.multiselect("Select Word Count Bins", ['0-5', '6-10', '11-20', '21-50', '51-100', '101+'], default=['0-5', '6-10', '11-20', '21-50', '51-100', '101+'])
-        
-        df['word_count'] = df['text'].dropna().apply(lambda x: len(x.split()))
-        bins = [0, 5, 10, 20, 50, 100, np.inf]
-        labels = ['0-5', '6-10', '11-20', '21-50', '51-100', '101+']
-        
-        df['word_count_bin'] = pd.cut(df['word_count'], bins=bins, labels=labels, right=False)
-        word_count_bins = df[df['word_count_bin'].isin(bin_labels)]['word_count_bin'].value_counts().sort_index()
-        
-        fig1, ax1 = plt.subplots()
-        sns.barplot(x=word_count_bins.index, y=word_count_bins.values, palette='viridis', ax=ax1)
-        ax1.set_title("Distribution of Word Counts in Feedback")
-        ax1.set_xlabel("Word Count Range")
-        ax1.set_ylabel("Number of Responses")
-        for p in ax1.patches:
-            ax1.annotate(f'{int(p.get_height())}', (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='baseline')
-        st.pyplot(fig1)
-        st.subheader('3. Meaningless Responses Check')
-        st.write('This section identifies responses that are meaningless (e.g., consist only of numbers, whitespace, or special characters).')
-        df['is_meaningless'] = df['text'].apply(lambda x: is_meaningless(str(x)) if pd.notna(x) else False)
-        meaningless_count = df['is_meaningless'].sum()
-        st.write(f"Number of meaningless responses: {meaningless_count}")
 
-        st.subheader('4. Extractive Summary')
-        st.write('This section provides an extractive summary by selecting the first sentence from the concatenated feedback text.')
-        all_texts = " ".join(df['text'].astype(str))
-        extractive_summary = extractive_summarize(all_texts)
-        st.write("Extractive Summary:")
-        st.write(extractive_summary)
-
-        st.subheader('5. Top Most Important Words Using TF-IDF')
-        top_n_words = st.slider('Select Top N Words', 5, 50, 20)
-        
-        tfidf_matrix = tfidf_vectorizer.fit_transform(feedback_text)
-        feature_names = tfidf_vectorizer.get_feature_names_out()
-        tfidf_scores = np.sum(tfidf_matrix.toarray(), axis=0)
-       
+        # Continue the rest of the code...
